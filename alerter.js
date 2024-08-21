a83a711668c6cb861732bd8f8f855700558e0596
NathiPrakash17 <manitamiltkv1997@gmail.com>
2024-08-21 20:06:26 +0530

    Update alerter.js

--- alerter.js
+++ alerter.js
@@ -2,25 +2,21 @@ let alertFailureCount = 0;
 
 function networkAlertStub(celcius) {
     console.log(`Alert! Temperature is ${celcius} degrees`);
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return 200;
+    return celcius>100?500:200;
 }
 
 function alertInCelcius(farenheit) {
     const celcius = (farenheit - 32) * 5 / 9;
     const returnCode = networkAlertStub(celcius);
     if (returnCode != 200) {
-        // non-ok response is not an error! Issues happen in life!
-        // let us keep a count of failures to report
-        // However, this code doesn't count failures!
-        // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
 }
 
 alertInCelcius(400.5);
 alertInCelcius(303.6);
+alertInCelcius(100.0);
 console.log(`${alertFailureCount} alerts failed.`);
+
+console.assert(alertFailureCount === 2 );
 console.log('All is well (maybe!)');
